6aa91b031abe5b3dcf460c6a79b4ca483a05a505
Jeff Ploughman <jeff_ploughman@troweprice.com>
2016-10-14 18:47:47 -0400

    Health check

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ func main() {
 
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/hello/{name}", index).Methods("GET")
+	router.HandleFunc("/health", index).Methods("GET")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
@@ -23,5 +24,74 @@ func index(w http.ResponseWriter, r *http.Request) {
 	name := vars["name"]
 
 	w.WriteHeader(http.StatusOK)
+	fmt.Fprintln(w, "Hello:", name)
+}
+
+func health(w http.ResponseWriter, r *http.Request) {
+	log.Println("Responsing to /health request")
+	log.Println(r.UserAgent())
+	rand.Seed(422)
+	answers := []int{
+	http.StatusContinue
+	,http.StatusSwitchingProtocols
+	,http.StatusProcessing
+	,http.StatusOK
+	,http.StatusCreated
+	,http.StatusAccepted
+	,http.StatusNonAuthoritativeInfo
+	,http.StatusNoContent
+	,http.StatusResetContent
+	,http.StatusPartialContent
+	,http.StatusMultiStatus
+	,http.StatusAlreadyReported
+	,http.StatusIMUsed
+	,http.StatusMultipleChoices
+	,http.StatusMovedPermanently
+	,http.StatusFound
+	,http.StatusSeeOther
+	,http.StatusNotModified
+	,http.StatusUseProxy
+	,http.StatusTemporaryRedirect
+	,http.StatusPermanentRedirect
+	,http.StatusBadRequest
+	,http.StatusUnauthorized
+	,http.StatusPaymentRequired
+	,http.StatusForbidden
+	,http.StatusNotFound
+	,http.StatusMethodNotAllowed
+	,http.StatusNotAcceptable
+	,http.StatusProxyAuthRequired
+	,http.StatusRequestTimeout
+	,http.StatusConflict
+	,http.StatusGone
+	,http.StatusLengthRequired
+	,http.StatusPreconditionFailed
+	,http.StatusRequestEntityTooLarge
+	,http.StatusRequestURITooLong
+	,http.StatusUnsupportedMediaType
+	,http.StatusRequestedRangeNotSatisfiable
+	,http.StatusExpectationFailed
+	,http.StatusTeapot
+	,http.StatusUnprocessableEntity
+	,http.StatusLocked
+	,http.StatusFailedDependency
+	,http.StatusUpgradeRequired
+	,http.StatusPreconditionRequired
+	,http.StatusTooManyRequests
+	,http.StatusRequestHeaderFieldsTooLarge
+	,http.StatusUnavailableForLegalReasons
+	,http.StatusInternalServerError
+	,http.StatusNotImplemented
+	,http.StatusBadGateway
+	,http.StatusServiceUnavailable
+	,http.StatusGatewayTimeout
+	,http.StatusHTTPVersionNotSupported
+	,http.StatusVariantAlsoNegotiates
+	,http.StatusInsufficientStorage
+	,http.StatusLoopDetected
+	,http.StatusNotExtended
+	,http.StatusNetworkAuthenticationRequired}
+
+	w.WriteHeader(answers[rand.Intn(len(answers))])
 	fmt.Fprintln(w, "Hello:", name)
 }
